1c2b2b690069842ba96a297d01bcf04d79135f04
Rasbats <miker@members.swis.net>
2021-04-19 12:22:33 +0100

    Correct declaration

--- src/otidalplanUIDialog.h
+++ src/otidalplanUIDialog.h
@@ -311,7 +311,7 @@ public:
 	wxString rte_end;
 	
 	void Addpoint(TiXmlElement* Route, wxString ptlat, wxString ptlon, wxString ptname, wxString ptsym, wxString pttype, wxString ptviz, wxString ptTime);
-	void otidalplanUIDialog::getTidalCurrentStation(double m_lat, double m_lon);
+	void getTidalCurrentStation(double m_lat, double m_lon);
 
 	wxString thisRoute;
 
