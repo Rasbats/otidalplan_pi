a06517c9a5e1ba635f3f675a9de5ba01692a0908
Rasbats <miker@members.swis.net>
2024-02-20 17:24:57 +0000

    v2.1.1
    Move to api-118 to make comatible with OpenCPN 5.8.4

--- src/otidalplan_pi.cpp
+++ src/otidalplan_pi.cpp
@@ -62,7 +62,7 @@ extern "C" DECL_EXP void destroy_pi(opencpn_plugin *p) { delete p; }
 //
 //---------------------------------------------------------------------------------------------------------
 
-otidalplan_pi::otidalplan_pi(void *ppimgr) : opencpn_plugin_116(ppimgr) {
+otidalplan_pi::otidalplan_pi(void *ppimgr) : opencpn_plugin_118(ppimgr) {
   // Create the PlugIn icons
   initialize_images();
 
